cd17f0779a2c200f1f69cc194ad8553b343eaad2
Christian Soares Santos <christiansoares2009@gmail.com>
2024-05-12 20:14:06 -0300

    update terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,19 +3,3 @@ variable "aws_region" {
   type    = string
   default = null
 }
-
-variable "db_username" {
-  description = "Username for the RDS database instance"
-  type        = string
-}
-
-variable "db_password" {
-  description = "Password for the RDS database instance"
-  type        = string
-  sensitive   = true
-}
-
-variable "database_name" {
-  description = "Database name for the RDS database instance"
-  type        = string
-}
